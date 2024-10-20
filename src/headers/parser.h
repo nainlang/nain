8955e8db40441d57509eea0a8686e763746a833b
Mario Rosell <hyper1516gamer@gmail.com>
2024-10-20 13:35:31 +0200

    Add parser

---
+++ src/headers/parser.h
@@ -0,0 +1,28 @@
+/*
+########################################
+### Nain programming language header ###
+########################################
+*/
+
+#ifndef LEXER_H
+#define LEXER_H
+#include "./header.h"
+#include <stdio.h>
+// OPERATION PARSING (priority 10)
+
+struct opNode {
+  char op;
+}
+
+int createNode() {
+  // TODO: implement this function
+}
+
+opList getIfOp(char *input, int idx) {
+  switch input[idx] {
+    case '+':
+      return
+  }
+}
+
+#endif
